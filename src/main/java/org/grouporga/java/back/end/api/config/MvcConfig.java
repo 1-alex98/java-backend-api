991d5bdaa4a1a00b46f4f330c7953157750ef0a6
Alexander von Trostorff <axel1200@users.noreply.github.com>
2019-04-05 17:35:52 +0200

    Fix login page

---
+++ src/main/java/org/grouporga/java/back/end/api/config/MvcConfig.java
@@ -0,0 +1,35 @@
+package org.grouporga.java.back.end.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.*;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/login").setViewName("login");
+    registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    registry.addResourceHandler("/favicon.ico").addResourceLocations("/");
+    registry.addResourceHandler("/robots.txt").addResourceLocations("/");
+  }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    configurer.setUseRegisteredSuffixPatternMatch(true);
+  }
+
+  @Override
+  public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+    // Turn off suffix-based content negotiation
+    configurer.favorPathExtension(false);
+  }
+}
