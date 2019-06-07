70b026efb7d9b47da7ccc4ae2e1a52980f659a6f
axel12 <alexander.von.trostorff@gmail.com>
2019-06-07 14:47:56 +0200

    Add me Controller
    Fixes #3

--- database/start.sh
+++ database/start.sh
@@ -1,2 +1,3 @@
 #bin/sh
 docker run --mount src="db",target="/var/lib/postgresql/data",type=volume -p "127.0.0.1:5432:5432" --restart=always --name orga-postgres -e POSTGRES_PASSWORD=oragne -d postgres
+docker run --network host --restart=always --env-file config/backend.env --name java-backend grouporga/java-backend:master
